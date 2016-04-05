acb888588463a01ca7ec98f77cd37399b87c11d0
kerilpalych <jonjo.shelvey33@gmail.com>
2016-04-06 00:19:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 
-function chgCol() {
-  $("#chtext").css("col", $("color").val())
+function newwind() {
+    windop = window.open("http://www.liverpoolfc.ru", "GOOGLE", "height=200,width=500");
+}
+function url() {
+    alert(location.href);
+}
+function chgBGclr() {
+    if (document.body.style.background.valueOf() === new String("orange").valueOf()) {
+        document.body.style.background = "";
+    } else {
+        document.body.style.background = "orange";
+    }
 }
