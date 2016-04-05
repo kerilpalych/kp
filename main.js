2109df1b8bdf974f83c2760002e5b0bb91b14355
kerilpalych <jonjo.shelvey33@gmail.com>
2016-04-06 01:07:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 function newwind() {
-    windop = window.open("http://www.liverpoolfc.ru", "GOOGLE", "height=200,width=500");
+    windop = window.open("http://www.liverpoolfc.ru", "LFC", "height=200,width=500");
 }
 function url() {
     alert(location.href);
